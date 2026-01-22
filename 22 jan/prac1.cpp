c1848b60c37228104e54a4fae28c085f56c1f9a0
IamGaurav001 <hellogauravdubey@gmail.com>
2026-01-22 16:23:27 +0530

    Updated OP

--- 22 jan/prac1.cpp
+++ 22 jan/prac1.cpp
@@ -1,17 +1,46 @@
 #include <iostream>
 using namespace std;
-int main()  {
-    int a;
-    int b;
-    cin >> a >> b;
-    int add = a + b;
-    int sub = a - b;
-    int mul = a * b;
-    int div = a / b;
-    int mod = a % b;
-    int leftShitf = a << 2;
-    int rightShift = a >> 2;
 
-    cout << add << " " << sub << " " << mul << " " << div << " " << mod << " " << leftShitf << " " << rightShift << endl;
+int main() {
+    int a, b;
+    char op;
+
+    cin >> a >> b;
+    cin >> op;  
+
+    if (op == '+') {
+        cout << a + b;
+    }
+    else if (op == '-') {
+        cout << a - b;
+    }
+    else if (op == '*') {
+        cout << a * b;
+    }
+    else if (op == '/') {
+        if (b != 0)
+            cout << a / b;
+        else
+            cout << "Invalid";
+    }
+    else if (op == '%') {
+        if (b != 0)
+            cout << a % b;
+        else
+            cout << "Invalid";
+    }
+    else if (op == '<') {  
+        cout << (a << 2);
+    }
+    else if (op == '>') {  
+        cout << (a >> 2);
+    }
+    else if (op == '|') {
+        cout << (a | b);
+    }
+    else {
+        cout << "Invalid Op";
+    }
+
     return 0;
 }
