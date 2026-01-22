170e9377c94d6b38698550f3bb4d762862ef4335
IamGaurav001 <hellogauravdubey@gmail.com>
2026-01-22 15:51:17 +0530

    Operator

--- 22 jan/prac1.cpp
+++ 22 jan/prac1.cpp
@@ -1,6 +1,13 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
-int main() {
-    cout << "Hello, World!" << endl;
+int main()  {
+    int a;
+    int b;
+    cin >> a >> b;
+    int add = a + b;
+    int sub = a - b;
+    int mul = a * b;
+    int div = a / b;
+    cout << add << " " << sub << " " << mul << " " << div << endl;
     return 0;
 }
